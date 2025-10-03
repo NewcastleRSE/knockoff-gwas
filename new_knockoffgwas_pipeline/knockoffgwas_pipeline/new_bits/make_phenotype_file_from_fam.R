e37f2114bb854c7a75e17dfdf69de7bd12355d0e
Dr Richard Howey <richard.howey@ncl.ac.uk>
2025-10-03 14:28:12 +0100

    Phenotype from .fam script added

---
+++ new_knockoffgwas_pipeline/knockoffgwas_pipeline/new_bits/make_phenotype_file_from_fam.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+#
+# Richard Howey
+# October 2025
+
+# Input arguments
+args <- commandArgs(trailingOnly=TRUE)
+chr <- as.numeric(args[1])
+geno.basename <- as.character(args[2])
+phenotype.name <- as.character(args[3])
+
+cat("Creating phenotype file using .fam file from chromesome ",chr, " with phenotype name ",phenotype.name," for use in KnockOffGWAS pipeline...\n")
+
+all_snps<-c()
+write_all<-FALSE
+
+# Read in .fam file
+fam.filename<-paste0(geno.basename,"_chr",chr,".fam")
+fam<-read.file(fam.filename, header=FALSE)
+
+# Create data to write to phenotype file
+header=c("FID",	"IID", "sex", phenotype.name)
+pheno.data<-fam[,c(1,2,5,6)]
+colnames(pheno.data)<-header
+  
+# Write new phenotype name
+phenotype.filename<-paste0(geno.basename,"_phenotypes.txt")
+write.table(pheno.data, row.names=FALSE, col.names=TRUE, quote=FALSE)
--- pbc_analysis/run_pre_pbc_analysis.sh
+++ pbc_analysis/run_pre_pbc_analysis.sh
@@ -1,6 +1,5 @@
-# Put in some SNP names, example data did not have SNP allele names, just 0 and 0 which are the same! So conversion to vcf failed
-sed -i -e 's/\t0\t0/\tT\tG/g' example_chr21.bim
-sed -i -e 's/\t0\t0/\tT\tG/g' example_chr22.bim
+# Create phenotype from fam file
+../new_knockoffgwas_pipeline/knockoffgwas_pipeline/new_bits/make_phenotype_file_from_fam.R 21 data/Nicola pbc
 
-# Run the example dataset with one simple script called from anywhere
-../new_knockoffgwas_pipeline/run_pre_knockoff_gwas.sh 21 22 example y 0.1 results
+# Run the PBC dataset with one simple script called from anywhere, see script for parameters
+../new_knockoffgwas_pipeline/run_pre_knockoff_gwas.sh 21 22 data/Nicola y 0.1 results
