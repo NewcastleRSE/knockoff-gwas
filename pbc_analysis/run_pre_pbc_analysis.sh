202d39649852de2e7fb6fdc0d20a394e5e2ad806
Dr Richard Howey <richard.howey@ncl.ac.uk>
2025-10-03 14:43:27 +0100

    Rscript call

--- pbc_analysis/run_pre_pbc_analysis.sh
+++ pbc_analysis/run_pre_pbc_analysis.sh
@@ -1,5 +1,5 @@
 # Create phenotype from fam file
-../new_knockoffgwas_pipeline/knockoffgwas_pipeline/new_bits/make_phenotype_file_from_fam.R 21 data/Nicola pbc
+Rscript --vanilla "../new_knockoffgwas_pipeline/knockoffgwas_pipeline/new_bits/make_phenotype_file_from_fam.R" 21 data/Nicola pbc
 
 # Run the PBC dataset with one simple script called from anywhere, see script for parameters
 ../new_knockoffgwas_pipeline/run_pre_knockoff_gwas.sh 21 22 data/Nicola y 0.1 results
