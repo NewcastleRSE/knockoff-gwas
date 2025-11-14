96cffe24c9286a96aa97360071b2ff2080a0624c
Dr Richard Howey <richard.howey@ncl.ac.uk>
2025-11-14 16:03:32 +0000

    Update set_dirs_unix.sh

--- pbc_analysis/set_dirs_unix.sh
+++ pbc_analysis/set_dirs_unix.sh
@@ -1,3 +1,3 @@
-# Run this file using "source set_dirs_comet.sh" so variables persist for use in all scripts
+# Run this file using "source set_dirs_unix.sh" so variables persist for use in all scripts
 DATA=data/
 IBD_DATA=idb_data/
