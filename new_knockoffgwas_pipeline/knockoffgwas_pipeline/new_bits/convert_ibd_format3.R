5ab14931635f8c2fa608055877c0a73745b0a9d6
Dr Richard Howey <richard.howey@ncl.ac.uk>
2025-12-05 17:31:05 +0000

    Create convert_ibd_format3.R

---
+++ new_knockoffgwas_pipeline/knockoffgwas_pipeline/new_bits/convert_ibd_format3.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+# Converts RaPIDv1.7 format to RaPIDv1.2.3
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) < 2) {
+  stop("Usage: Rscript convert_ibd_format.R <input_file> <output_file>\n")
+}
+
+infile  <- args[1]
+outfile <- args[2]
+
+# Read v1.7 file (space-delimited, no header)
+df <- read.table(infile, header = FALSE, stringsAsFactors = FALSE)
+
+# Assign column names based on v1.7 format
+colnames(df) <- c(
+  "CHR",
+  "HAPID1",
+  "HAPID2",
+  "HID1",
+  "HID2",
+  "BP.start",
+  "BP.end",
+  "cM",
+  "site.start",
+  "site.end"
+)
+
+# Function to split HAPID into Family ID and Individual ID
+# Splits from middle "_" so if individual and family IDs are different
+# with a different number of "_" present then this will not be correct.
+split_fam_ind <- function(hapid) {
+  parts <- strsplit(hapid, "_")[[1]]
+  mid <- floor(length(parts) / 2)
+  fam <- paste(parts[1:mid], collapse = "_")
+  ind <- paste(parts[(mid+1):length(parts)], collapse = "_")
+  return(list(fam = fam, ind = ind))
+}
+
+# Extract FAM and ID for both haplotypes
+fam1_id1 <- lapply(df$HAPID1, split_fam_ind)
+fam2_id2 <- lapply(df$HAPID2, split_fam_ind)
+
+FAM1 <- sapply(fam1_id1, function(x) x$fam)
+ID1  <- sapply(fam1_id1, function(x) x$ind)
+
+FAM2 <- sapply(fam2_id2, function(x) x$fam)
+ID2  <- sapply(fam2_id2, function(x) x$ind)
+
+# Build output in RaPID v1.2.3 format
+out <- data.frame(
+  CHR        = df$CHR,
+  ID1        = ID1,
+  HID1       = df$HID1,
+  ID2        = ID2,
+  HID2       = df$HID2,
+  BP.start   = df$BP.start,
+  BP.end     = df$BP.end,
+  site.start = df$site.start,
+  site.end   = df$site.end,
+  cM         = df$cM,
+  FAM1       = rep(-1, dim(df)[1]),
+  FAM2       = rep(-1, dim(df)[1]),
+  stringsAsFactors = FALSE
+)
+
+# Write result
+write.table(out, outfile, sep = "\t", quote = FALSE, row.names = FALSE)
