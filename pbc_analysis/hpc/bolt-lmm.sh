23685a15bd244ed0a84ad45b35d3397dc4302ccb
Richard Howey <richard.howey@ncl.ac.uk>
2026-02-23 09:01:45 +0000

    BOLT dev

--- pbc_analysis/hpc/bolt-lmm.sh
+++ pbc_analysis/hpc/bolt-lmm.sh
@@ -3,11 +3,11 @@
 #SBATCH --account=comet_kogwas
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10GB
-#SBATCH --array=1-22                       # Run tasks for given chromosomes
+#SBATCH --array=22                       # Run tasks for given chromosomes
 #SBATCH --output=slurm_bolt_lmm_%a.out
 
 # Load modules
-module load BOLT-LMM/v2.4.1
+module load BOLT-LMM
 module load PLINK/1.9b_6.21-x86_64
 module load R/4.5.1-gfbf-2024a 
 
@@ -17,6 +17,6 @@ source ./set_dirs.sh
 date
 echo "Running on $HOSTNAME PBC analysis"
 
-../new_knockoffgwas_pipeline/run_lmm.sh $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID $DATA/Nicola pbc 0.1 results $DATA"/tables/LDSCORE.1000G_EUR.tab.gz" $DATA"/tables/genetic_map_hg19_withX.txt.gz"
+../new_knockoffgwas_pipeline/run_lmm.sh $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID $DATA/Nicola pbc 0.1 results $DATA"/tables/LDSCORE.1000G_EUR.GRCh38.tab.gz" $DATA"/tables/genetic_map_hg19_withX.txt.gz"
 
 date
