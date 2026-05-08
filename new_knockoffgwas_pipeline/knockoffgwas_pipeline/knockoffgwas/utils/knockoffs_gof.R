cd50bbbaf35d6918ea88cdf45c18fd01c549fb9a
Dr Richard Howey <richard.howey@ncl.ac.uk>
2026-05-08 16:41:15 +0100

    R namespaces for container

--- new_knockoffgwas_pipeline/knockoffgwas_pipeline/knockoffgwas/utils/knockoffs_gof.R
+++ new_knockoffgwas_pipeline/knockoffgwas_pipeline/knockoffgwas/utils/knockoffs_gof.R
@@ -23,25 +23,25 @@ out.basename  <- as.character(args[5])
 plot.knockoff.diagnostics <- function(chr.name, res.name, stats.basename, groups.file) {
 
     ## Load variable grouping
-    Variants <- read_delim(groups.file, delim=" ", progress = FALSE, col_types = cols()) %>%
-        mutate(Group = as.integer(Group))
+    Variants <- read_delim(groups.file, delim=" ", progress = FALSE, col_types = readr::cols()) %>%
+        dplyr::mutate(Group = as.integer(Group))
 
     ## Load variant frequency table
     frq.file <- sprintf("%s_self.frq", stats.basename)
-    Frq <- read_table(frq.file, col_types=cols())
+    Frq <- readr::read_table(frq.file, col_types=readr::cols())
     ## Compute diagnostics
     Frq <- Frq %>%
-        separate(SNP, ".k", into=c("SNP", "Knockoff")) %>%
-        mutate(Knockoff = ifelse(is.na(Knockoff), FALSE, TRUE))
+        dplyr::separate(SNP, ".k", into=c("SNP", "Knockoff")) %>%
+        dplyr::mutate(Knockoff = ifelse(is.na(Knockoff), FALSE, TRUE))
     Diagnostics <- Frq %>%
-        pivot_wider(names_from="Knockoff", values_from=c("MAF")) %>%
-        mutate(x=`FALSE`, xk=`TRUE`) %>%
-        select(CHR, SNP, x, xk)
+        tidyr::pivot_wider(names_from="Knockoff", values_from=c("MAF")) %>%
+        dplyr::mutate(x=`FALSE`, xk=`TRUE`) %>%
+        dplyr::select(CHR, SNP, x, xk)
     ## Plot frequency diagnostics
     p.frq <- Diagnostics %>%
-        ggplot(aes(x=x, y=xk)) +
-        geom_point(alpha=0.2) +
-        geom_abline(intercept = 0, slope = 1, color="red", linetype=2) +
+        ggplot2::ggplot(aes(x=x, y=xk)) +
+        ggplot2::geom_point(alpha=0.2) +
+        ggplot2::geom_abline(intercept = 0, slope = 1, color="red", linetype=2) +
         xlab(TeX("Allele-1 frequency ($X$)")) + ylab(TeX("Allele-1 frequency ($\\tilde{X}$)")) +
         xlim(0,1) +
         ylim(0,1) +
@@ -49,81 +49,81 @@ plot.knockoff.diagnostics <- function(chr.name, res.name, stats.basename, groups
     
     ## Load LD table
     ld.file <- sprintf("%s.ld", stats.basename)
-    LD <- suppressWarnings(read_table2(ld.file, col_types=cols())) %>%
-        mutate(CHR=CHR_A) %>% select(-CHR_A, -CHR_B)
+    LD <- suppressWarnings(readr::read_table2(ld.file, col_types=readr::cols())) %>%
+        dplyr::mutate(CHR=CHR_A) %>% select(-CHR_A, -CHR_B)
 
     ## Add grouping information
     LD <- LD %>%
-        mutate(SNP=SNP_A) %>%
-        mutate(SNP = gsub(".k", "", SNP)) %>%
-        inner_join(Variants %>% select(SNP, Group), by = c("SNP")) %>%
-        mutate(Group_A=Group) %>% select(-SNP, -Group) %>%
-        mutate(SNP=SNP_B) %>%
-        mutate(SNP = gsub(".k", "", SNP)) %>%
-        inner_join(Variants %>% select(SNP, Group), by = c("SNP")) %>%
-        mutate(Group_B=Group) %>% select(-SNP, -Group)
+        dplyr::mutate(SNP=SNP_A) %>%
+        dplyr::mutate(SNP = gsub(".k", "", SNP)) %>%
+        dplyr::inner_join(Variants %>% dplyr::select(SNP, Group), by = c("SNP")) %>%
+        dplyr::mutate(Group_A=Group) %>% dplyr::select(-SNP, -Group) %>%
+        dplyr::mutate(SNP=SNP_B) %>%
+        dplyr::mutate(SNP = gsub(".k", "", SNP)) %>%
+        dplyr::inner_join(Variants %>% dplyr::select(SNP, Group), by = c("SNP")) %>%
+        dplyr::mutate(Group_B=Group) %>% dplyr::select(-SNP, -Group)
 
     ## Add knockoff key information
     LD <- LD %>%
-        separate(SNP_A, ".k", into=c("SNP_A", "Knockoff_A")) %>%
-        mutate(Knockoff_A = ifelse(is.na(Knockoff_A), FALSE, TRUE)) %>%
-        separate(SNP_B, ".k", into=c("SNP_B", "Knockoff_B")) %>%
-        mutate(Knockoff_B = ifelse(is.na(Knockoff_B), FALSE, TRUE))
+        dplyr::separate(SNP_A, ".k", into=c("SNP_A", "Knockoff_A")) %>%
+        dplyr::mutate(Knockoff_A = ifelse(is.na(Knockoff_A), FALSE, TRUE)) %>%
+        dplyr::separate(SNP_B, ".k", into=c("SNP_B", "Knockoff_B")) %>%
+        dplyr::mutate(Knockoff_B = ifelse(is.na(Knockoff_B), FALSE, TRUE))
 
     # Create correlation tables between different groups
     group.range <- seq(0,10)
     LD.XX <- LD %>%
-        filter(abs(Group_B-Group_A) %in% group.range, Knockoff_A==FALSE, Knockoff_B==FALSE) %>%
-        mutate(R.XX=R2) %>%
-        mutate(SNP_A=str_replace(SNP_A,".A",""), SNP_A=str_replace(SNP_A,".B","")) %>%
-        mutate(SNP_B=str_replace(SNP_B,".A",""), SNP_B=str_replace(SNP_B,".B","")) %>%
-        select(Group_A, Group_B, SNP_A, SNP_B, R.XX) %>%
-        distinct(Group_A, Group_B, SNP_A, SNP_B, R.XX)
+        dplyr::filter(abs(Group_B-Group_A) %in% group.range, Knockoff_A==FALSE, Knockoff_B==FALSE) %>%
+        dplyr::mutate(R.XX=R2) %>%
+        dplyr::mutate(SNP_A=stringr::str_replace(SNP_A,".A",""), SNP_A=stringr::str_replace(SNP_A,".B","")) %>%
+        dplyr::mutate(SNP_B=stringr::str_replace(SNP_B,".A",""), SNP_B=stringr::str_replace(SNP_B,".B","")) %>%
+        dplyr::select(Group_A, Group_B, SNP_A, SNP_B, R.XX) %>%
+        dplyr::distinct(Group_A, Group_B, SNP_A, SNP_B, R.XX)
     LD.XkXk <- LD %>%
-        filter(abs(Group_B-Group_A) %in% group.range, Knockoff_A==TRUE, Knockoff_B==TRUE) %>%
-        mutate(R.XkXk=R2) %>%
-        mutate(SNP_A=str_replace(SNP_A,".A",""), SNP_A=str_replace(SNP_A,".B","")) %>%
-        mutate(SNP_B=str_replace(SNP_B,".A",""), SNP_B=str_replace(SNP_B,".B","")) %>%
-        select(Group_A, Group_B, SNP_A, SNP_B, R.XkXk) %>%
-        distinct(Group_A, Group_B, SNP_A, SNP_B, R.XkXk)
+        dplyr::filter(abs(Group_B-Group_A) %in% group.range, Knockoff_A==TRUE, Knockoff_B==TRUE) %>%
+        dplyr::mutate(R.XkXk=R2) %>%
+        dplyr::mutate(SNP_A=stringr::str_replace(SNP_A,".A",""), SNP_A=stringr::str_replace(SNP_A,".B","")) %>%
+        dplyr::mutate(SNP_B=stringr::str_replace(SNP_B,".A",""), SNP_B=stringr::str_replace(SNP_B,".B","")) %>%
+        dplyr::select(Group_A, Group_B, SNP_A, SNP_B, R.XkXk) %>%
+        dplyr::distinct(Group_A, Group_B, SNP_A, SNP_B, R.XkXk)
     LD.XXk <- LD %>%
-        filter((Group_B-Group_A) %in% seq(1,10), Knockoff_A*Knockoff_B==FALSE) %>%
-        mutate(R.XXk=R2) %>%
-        mutate(SNP_A=str_replace(SNP_A,".A",""), SNP_A=str_replace(SNP_A,".B","")) %>%
-        mutate(SNP_B=str_replace(SNP_B,".A",""), SNP_B=str_replace(SNP_B,".B","")) %>%
-        select(Group_A, Group_B, SNP_A, SNP_B, R.XXk) %>%
-        distinct(Group_A, Group_B, SNP_A, SNP_B, R.XXk)
+        dplyr::filter((Group_B-Group_A) %in% seq(1,10), Knockoff_A*Knockoff_B==FALSE) %>%
+        dplyr::mutate(R.XXk=R2) %>%
+        dplyr::mutate(SNP_A=stringr::str_replace(SNP_A,".A",""), SNP_A=stringr::str_replace(SNP_A,".B","")) %>%
+        dplyr::mutate(SNP_B=stringr::str_replace(SNP_B,".A",""), SNP_B=stringr::str_replace(SNP_B,".B","")) %>%
+        dplyr::select(Group_A, Group_B, SNP_A, SNP_B, R.XXk) %>%
+        dplyr::distinct(Group_A, Group_B, SNP_A, SNP_B, R.XXk)
 
     ## Plot originality
-    LD.cross <- inner_join(LD.XX, LD.XkXk, by = c("Group_A", "Group_B", "SNP_A", "SNP_B"))
+    LD.cross <- dplyr::inner_join(LD.XX, LD.XkXk, by = c("Group_A", "Group_B", "SNP_A", "SNP_B"))
     p.orig <- LD.cross %>%
-        mutate(Distance = as.factor(abs(Group_A-Group_B))) %>%
-        ggplot(aes(x=abs(R.XX), y=abs(R.XkXk))) +
-        geom_abline(color="red") +
-        geom_point(alpha=0.1) +
+        dplyr::mutate(Distance = as.factor(abs(Group_A-Group_B))) %>%
+        ggplot2::ggplot(aes(x=abs(R.XX), y=abs(R.XkXk))) +
+        ggplot2::geom_abline(color="red") +
+        ggplot2::geom_point(alpha=0.1) +
         xlim(0,1) + ylim(0,1) +
         xlab(TeX("|corr($X_{j},X_{k}$)|")) + ylab(TeX("|corr($\\tilde{X}_{j},\\tilde{X}_{k}$)|")) +
         theme_bw()
     
     ## Plot exchangeability
     options(repr.plot.width=4, repr.plot.height=3)
-    LD.cross <- inner_join(LD.XX, LD.XXk, by = c("Group_A", "Group_B", "SNP_A", "SNP_B")) %>%
-        filter(Group_A!=Group_B)
+    LD.cross <- dplyr::inner_join(LD.XX, LD.XXk, by = c("Group_A", "Group_B", "SNP_A", "SNP_B")) %>%
+        dplyr::filter(Group_A!=Group_B)
     p.exch <- LD.cross %>%
-        mutate(Distance = as.factor(abs(Group_A-Group_B))) %>%
-        ggplot(aes(x=abs(R.XX), y=abs(R.XXk))) +
-        geom_abline(color="red") +
-        geom_point(alpha=0.1) +
+        dplyr::mutate(Distance = as.factor(abs(Group_A-Group_B))) %>%
+        ggplot2::ggplot(aes(x=abs(R.XX), y=abs(R.XXk))) +
+        ggplot2::geom_abline(color="red") +
+        ggplot2::geom_point(alpha=0.1) +
         xlim(0,1) + ylim(0,1) +
         xlab(TeX("|corr($X_{j},X_{k}$)|")) + ylab(TeX("|corr($X_{j},\\tilde{X}_{k}$)|")) +
         theme_bw()
     
     ## Plot histogram of self-correlations
-    p.self <- LD %>% filter(BP_A==BP_B) %>%
-        ggplot(aes(x=R2)) +
-        geom_histogram(bins=30) +
-        xlab(TeX("|corr($X_{j},\\tilde{X}_{j}$)|")) +
-        theme_bw()
+    p.self <- LD %>% dplyr::filter(BP_A==BP_B) %>%
+        ggplot2::ggplot(aes(x=R2)) +
+        ggplot2::geom_histogram(bins=30) +
+        ggplot2::xlab(TeX("|corr($X_{j},\\tilde{X}_{j}$)|")) +
+        ggplot2::theme_bw()
 
     ## Combine plots
     plot.title <- sprintf("Knockoff GOF for chromosome %s, resolution %s", chr.name, res.name)
@@ -137,5 +137,5 @@ pp <- plot.knockoff.diagnostics(chr.name, res.name, stats.basename, groups.file)
 
 ## Save plot
 out.file <- sprintf("%s.png", out.basename)
-ggsave(out.file, plot=pp)
+ggplot2::ggsave(out.file, plot=pp)
 cat(sprintf("GOF plots saved on %s\n", out.file))
