2f6607fcf102104d22e3121a459fa2383435c57f
Dr Richard Howey <richard.howey@ncl.ac.uk>
2026-05-08 11:28:42 +0100

    test

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+print("SOURCE DIR:", os.getcwd())
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
