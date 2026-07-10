abc3f3da259164bf07f50a09bdf955421df8b19b
Richard Howey <richard.howey@ncl.ac.uk>
2026-07-10 12:15:46 +0100

    partitions with summary stats added to logs

---
+++ new_knockoffgwas_pipeline/run_knockoff_gwas_part_only.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Class: script
+#
+# Run KnockoffGWAS on any dataset
+#
+# Authors: Richard Howey
+# Date:    28/03/2025
+#
+# Parameters
+#
+# $1 = start chr number
+# $2 = path & file prefix (not including "_chrXX") for genetic data, .bim, .bed, .fam
+#      also path & file prefix (not including "_map_chrXX") for genetic map data, .txt
+#      also path & file prefix (not including "_ibd_chrXX") for IBD data, .txt
+#      also path & file prefix (not including "_phenotypes") for all phenotype data, .txt
+#      also path & file prefix (not including "_qc_chrXX") for QC SNP data, .txt
+#      also path & file prefix (not including "_qc_variants") for all QC SNP data, .txt
+#      also path & file prefix (not including "_qc_samples") for all QC SNP data, .txt
+# $3 = phenotype name
+# $4 = FDR rate
+# $5 = output folder
+
+# Absolute path to this script
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+# Set data dir
+DATA=$(dirname "$2")
+
+# Temporary folder for temporary files
+TMP_DIR=$DATA"/tmp"
+mkdir -p $5
+mkdir -p $TMP_DIR
+
+$SCRIPTPATH/knockoffgwas_pipeline/analyze_part_only.sh $1 $1 $2 $3 $4 $5 
+
+
---
+++ pbc_analysis/hpc/anal_part_only.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=default_free
+#SBATCH --account=comet_kogwas
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100GB
+# --time=14-00:00:00
+#SBATCH --array=1-22                       # Run tasks for given chromosomes
+#SBATCH --output=slurm_part_only_%a_FDR20.out
+
+# Load modules
+
+module load BCFtools/1.22-GCC-13.3.0
+module load PLINK/1.9b_6.21-x86_64
+module load R/4.5.1-gfbf-2024a 
+
+# Set dirs
+source ./set_dirs.sh
+
+date
+echo "Running on $HOSTNAME PBC analysis"
+
+../new_knockoffgwas_pipeline/run_knockoff_gwas_part_only.sh $SLURM_ARRAY_TASK_ID $DATA/Nicola pbc 0.1 results_d25_w3_FDR10
+
+date
